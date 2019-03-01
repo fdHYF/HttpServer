7f91d6c29960ec856e333c0d63ed30089e7d9f04
hyf <37368470+fdHYF@users.noreply.github.com>
2019-03-01 20:22:21 +0800

    Update ThreadPool.h

--- HttpServer/ThreadPool.h
+++ HttpServer/ThreadPool.h
@@ -6,21 +6,23 @@
 #include <list>
 #include <memory>
 #include <functional>
-
+#include "Request.h"
+using sp_httpdata = std::shared_ptr<HttpData>;
 const int MAX_THREAD_NUM = 8;
 const int MAX_REQUEST_NUM = 65535;
 
-//线程池任务类
+//绾跨▼姹犱换鍔＄被
 struct RequestTask {
-	std::function<void()> func;
-	//void* args;
+    std::function<void(sp_httpdata, int)> func;
+    sp_httpdata data;
+    int event;
 };
 
 class ThreadPool {
 public:
-	explicit ThreadPool(const int& thread_number = 4, const int& request_number = 10000);
+	explicit ThreadPool(const int& thread_number = 1, const int& request_number = 10000);
 	~ThreadPool() { stopped_ = true; }
-	void append(RequestTask* request);
+	void append(RequestTask request);
 
 private:
 	static void* worker(void* arg);
@@ -32,5 +34,5 @@ private:
 	MutexLock mutex_;
 	Condition cond_;
 	std::vector<pthread_t> threads;
-	std::list<RequestTask*> requests;
+	std::list<RequestTask> requests;
 };
